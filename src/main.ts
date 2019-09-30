100a3b68884752e4f541c6163f16f9e21206b1ff
wenbin.zheng <3421736@qq.com>
2019-09-30 10:58:26 +0800

    optimize

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
 // px2rem
 window.onresize = setHtmlFontSize
-function setHtmlFontSize () {
+function setHtmlFontSize() {
   const htmlWidth = document.documentElement.clientWidth || document.body.clientWidth
   const htmlDom = document.getElementsByTagName('html')[0]
   htmlDom.style.fontSize = htmlWidth / 10 + 'px'
